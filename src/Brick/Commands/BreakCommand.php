8da847ce54634c33ea82fa9d6dab86481885bd89
Ilya S <public.ilya.s@gmail.com>
2014-04-05 15:08:40 +0700

    Add Brick\Commands\BreakCommand

---
+++ src/Brick/Commands/BreakCommand.php
@@ -0,0 +1,39 @@
+<?php namespace Brick\Commands;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class BreakCommand extends Command {
+
+    /**
+     * Configure the command
+     *
+     * @return void
+     */
+    protected function configure()
+    {
+        $this->setName('break');
+
+        $this->setDescription('Attempt to break the given class');
+
+        $this->setDefinition([
+            new InputArgument('class', InputArgument::REQUIRED, 'Full class name'),
+        ]);
+    }
+
+    /**
+     * Execute the command
+     *
+     * @param  Symfony\Component\Console\Input\InputInterface   $input
+     * @param  Symfony\Component\Console\Output\OutputInterface $output
+     * @return void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('Hello, world!');
+    }
+
+}
+
